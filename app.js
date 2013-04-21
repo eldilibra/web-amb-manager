6c56367afd82da78ec70e9d7c3b4a339b9773287
eldilibra <writeme@ldlibra.com>
2013-04-21 02:45:06 -0400

    sorry for the big commit: i got carried away

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var http = require('http');
+var util = require('./lib/util');
 var monitor = require('./lib/monitor');
+var command = require('./lib/command');
+var error_handler = require('./lib/error-handler');
 
 var app = express();
+app.use(util);
 app.use(monitor);
+app.use(command);
 
 app.set('port', process.env.port || 3000);
 app.use(app.router);
+app.use(error_handler);
 
 http.createServer(app.listen(app.get('port'), function () {
-  console.log('Web AMB Manager listening on', app.get('port'));
-});
+    console.log('Web AMB Manager listening on', app.get('port'));
+}));
